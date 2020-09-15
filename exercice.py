63ff7df4dd69f270651f903afe0424a8582125be
bASILgIL <basil.gil@polymtl.ca>
2020-09-15 05:31:52 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -13,7 +13,7 @@ def square(a: float) -> float:
 
 
 def average(a: float, b: float, c: float) -> float:
-    return ((sum(a, b, c))/3
+    return (a +/3
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
     return 
